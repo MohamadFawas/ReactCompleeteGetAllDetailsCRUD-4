e981e4c8281857eb4f81b3d92ba1c24e196810b3
MohamadFawas <mohamadfawas19971003@gmail.com>
2023-08-07 17:33:45 +0530

    fourign Key

---
+++ practiceCRUD_forignKey/src/main/java/com/crud/practiceCRUD/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.crud.practiceCRUD.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import com.crud.practiceCRUD.entity.Library_management;
+
+public interface BookRepository extends JpaRepository <Library_management, Long>{
+
+	public boolean existsByIndexNumberIgnoreCase(String indexNumber);
+	//public boolean exiexistsById(Long id);
+}
